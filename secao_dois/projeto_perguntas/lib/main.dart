bd42defc8a4b11abcfaf80a4ff4738659e3c0b91
higornobrega <higorst.nobrega@gmail.com>
2025-03-20 17:12:01 -0300

    first commit

---
+++ secao_dois/projeto_perguntas/lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import './questionario.dart';
+import './resultado.dart';
+
+void main() => runApp(const PerguntaApp());
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+  var _pontuacaoTotal = 0;
+  final _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': [
+        {'texto': 'Preto', 'pontuacao': 10},
+        {'texto': 'Vermelho', 'pontuacao': 5},
+        {'texto': 'Verde', 'pontuacao': 3},
+        {'texto': 'Branco', 'pontuacao': 1},
+      ],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': [
+        {'texto': 'Coelho', 'pontuacao': 10},
+        {'texto': 'Cobra', 'pontuacao': 5},
+        {'texto': 'Elefante', 'pontuacao': 3},
+        {'texto': 'Leão', 'pontuacao': 1},
+      ],
+    },
+    {
+      'texto': 'Qual é o seu instrutor favorito?',
+      'respostas': [
+        {'texto': 'Leo', 'pontuacao': 10},
+        {'texto': 'Maria', 'pontuacao': 5},
+        {'texto': 'João', 'pontuacao': 3},
+        {'texto': 'Pedro', 'pontuacao': 1},
+      ],
+    },
+  ];
+
+  void _responder(int pontuacao) {
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+        _pontuacaoTotal += pontuacao;
+      });
+    }
+    print(_pontuacaoTotal);
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Perguntas')),
+        body:
+            temPerguntaSelecionada
+                ? Questionario(
+                  perguntas: _perguntas,
+                  perguntaSelecionada: _perguntaSelecionada,
+                  quandoResponder: _responder,
+                )
+                : const Resultado(),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  const PerguntaApp({super.key});
+
+  @override
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+}
+
+// Fala, pessoal!
+
+// Na próxima aula o professor vai, novamente, dar uma refatorada no código e irá gerar um pequeno problema dentro do return de Resposta no arquivo questionario.dart.
+
+// Para resolver é bem simples, basta adicionar uma função toString() e int.parse() lá no return.
+
+// O código irá ficar dessa forma:
+
+// return Resposta(
+//             resp['texto'].toString(),
+//             () => quandoResponder(int.parse(resp['pontuacao'].toString())),
+//           );
+// Bons estudos!
