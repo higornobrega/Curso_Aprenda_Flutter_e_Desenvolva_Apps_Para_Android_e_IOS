c40fbaa67357e3c2ee941df93ffec366f1250b80
higornobrega <higorst.nobrega@gmail.com>
2025-04-10 16:56:57 -0300

    transaction

--- secao_quatro/expenses/lib/main.dart
+++ secao_quatro/expenses/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
@@ -14,8 +15,20 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,9 +44,61 @@ class MyHomePage extends StatelessWidget {
               child: Text('Gráfico'),
             ),
           ),
-          Card(child: Text('Lista de Transações')),
+          Column(
+            children:
+                _transactions.map((tr) {
+                  return Card(
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 10,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 2),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Text(tr.value.toString()),
+                        ),
+                        Column(
+                          children: [Text(tr.title), Text(tr.date.toString())],
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+          ),
         ],
       ),
     );
   }
 }
+
+
+// Correção Transaction e SDK
+// Fala galera!
+
+// Na próxima aula o professor vai criar a classe modelo Transaction, porém algumas mudanças são necessárias por conta do Flutter 2. Em resumo, não será mais necessário fazer o import do 'foundation' nem utilizar o @ no required.
+
+// O código ficará assim:
+
+// class Transaction {
+//   final String id;
+//   final String title;
+//   final double value;
+//   final DateTime date;
+ 
+//   Transaction({
+//     required this.id,
+//     required this.title,
+//     required this.value,
+//     required this.date,
+//   });
+// }
+// A mudança na versão do SDK para poder aceitar o operador spread também não é mais necessária, o operador irá funcionar normalmente desde que seu projeto tenha sido criado com as versões atualizadas do Dart e Flutter.
+
+// Como padrão o sdk estará com os seguintes números lá no arquivo pubspec:
+
+// sdk: ">=2.12.0 <3.0.0"
+// Bons estudos!
